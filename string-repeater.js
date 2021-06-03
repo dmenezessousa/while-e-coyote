a50cfbb4fa3e1feae07343ed42b3819385c5b014
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-06-03 12:28:30 -0400

    updated from for to while

--- string-repeater.js
+++ string-repeater.js
@@ -15,13 +15,19 @@
 //     console.log(pyramid);
 // }
 
-
+//import promt-sync
 const prompt = require('prompt-sync')();
+
+//get string from user
 let n = prompt("Enter a string:  ");
+
+//store string
 let pyramid = n;
 let count = 0;
-while (n.length < 10) {
+
+//display function
+while (n.length < 10) { //max string length
     console.log(n);
-    n = n + pyramid;
-    count ++;
+    n = n + pyramid; //add + repeat strings
+    count ++; // display in column 
 }
